632ac504da4f54d59890e4437ac3e3ce8705f740
lamaDayoub <lama1e2dayoub@gmail.com>
2026-02-13 02:08:17 +0300

    adding blog to the Blog table via post api and successfully connecting to the database via engine and sessionmaker

--- blog/main.py
+++ blog/main.py
@@ -1,10 +1,33 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Depends
 from . import schemas, models
-from .database import engine
+from .database import engine,SessionLocal
+from sqlalchemy.orm import Session 
+
 
 app = FastAPI()
+def get_db():
+    #Before yield: FastAPI runs this part before your route starts (creates the session).
+    #This creates a new, private session for a single request
+    db = SessionLocal()
+    try:
+        yield db
+    #even if the code crashes—always execute the code in finally
+    finally:
+        # Once your route has finished sending the response to the user, FastAPI "wakes up" #this function and runs the code after the yield (closing the session).
+        db.close()
+    
 
+
+#like migrate in django it tells to make all tables from models.py if they are not created #befor in the database
 models.Base.metadata.create_all(engine)
+
+#The Logic: This is a core FastAPI concept. that is saying: "Before you run this function, I #need you to go run get_db and give me a database session."
+#It ensures that every request gets its own database connection and, most importantly, closes #it when the request is finished.
 @app.post('/blog')
-def create (request : schemas.Blog):
-    return request
+def create (request : schemas.Blog, db : Session = Depends(get_db)):
+    # db: Session: This is your "link" to the database. You will use this variable to perform #your CRUD operations (like db.add() or db.query()).
+    new_blog = models.Blog(title = request.title , body = request .body)
+    db.add(new_blog)
+    db.commit()
+    db.refresh(new_blog)
+    return new_blog
