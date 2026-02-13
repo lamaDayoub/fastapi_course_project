ac6bc25204c367922c0e528da6c7fec693c1fb70
lamaDayoub <lama1e2dayoub@gmail.com>
2026-02-13 04:04:17 +0300

    add delete blog api

--- blog/main.py
+++ blog/main.py
@@ -32,6 +32,16 @@ def create (request : schemas.Blog, db : Session = Depends(get_db)):
     db.refresh(new_blog)
     return new_blog
 
+@app.delete('/blog/{id}',status_code=status.HTTP_204_NO_CONTENT)
+def delete_blog(id, db: Session = Depends(get_db)):
+    blog =db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        # This raises a 404 error if the blog isn't found
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
+                            detail=f"Blog with id {id} not found")
+    blog.delete(synchronize_session=False)
+    db.commit()
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 @app.get('/blog') #db : Session = Depends(get_db)  is a database instance
 def get_all(db : Session = Depends(get_db)):
